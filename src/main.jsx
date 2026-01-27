d198f61370edc45a6b1a24b4ceca1e1f7d28334a
arbabprvt-ofc <arbabprvt@gmail.com>
2026-01-27 23:15:11 +0530

    Add centralized logger

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './lib/axios'
+import { initLogger } from './lib/logger'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
+
+initLogger();
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
